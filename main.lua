5176166a0a73690e3b240c04eb34b5617b7429cb
Alex Haible <52239823+AlexHaible@users.noreply.github.com>
2025-06-26 12:50:52 +0200

    Prep for 11.2.0 and MoP Classic

--- main.lua
+++ main.lua
@@ -36,9 +36,19 @@ local defaults = {
 local configOptions
 
 -- Get the game version
+local isRetail  = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
 local isClassic = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
-local isCata = WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC
-local isRetail = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
+local isTBC     = WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC
+local isWotLK   = WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC
+local isCata    = WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC
+local isMists   = WOW_PROJECT_ID == WOW_PROJECT_MISTS_CLASSIC
+
+-- “Or above” helpers for gating
+local isMistsOrAbove   = isMists   or isRetail
+local isCataOrAbove    = isCata    or isMists or isRetail
+local isWotLKOrAbove   = isWotLK   or isCata  or isMists or isRetail
+local isTBCOrAbove     = isTBC     or isWotLK or isCata  or isMists or isRetail
+local isClassicOrAbove = isClassic or isTBC   or isWotLK or isCata  or isMists or isRetail
 
 -- Kill the category from different plugin.
 Categories:WipeCategory(Context:New('BBTeleporters_DeleteCategory'),TUTORIAL_TITLE31)
@@ -46,7 +56,7 @@ Categories:WipeCategory(Context:New('BBTeleporters_DeleteCategory'),TUTORIAL_TIT
 -- Make an empty table to store item data in...
 local teleporters = {}
 
-if isClassic or isCata then
+if isClassic or isTBC or isWotLK or isCata then
     defaults.profile = {
         enablePortalReagents = false,
     }
@@ -92,7 +102,7 @@ if isClassic or isCata then
 else
     configOptions = {
         retailOptions = {
-            name = L:G("Retail Options"),
+            name = L:G("Options"),
             type = "group",
             order = 1,
             inline = true,
@@ -131,37 +141,38 @@ function Teleporters:addTeleportItemsToTable()
     -- Clear the table of items if needed.
     table.wipe(teleporters)
 
-    if (isClassic or isCata) and db.enablePortalReagents then
+    if (isClassic or isTBC or isWotLK or isCata) and db.enablePortalReagents then
         -- Add the reagents for mage portals to the teleporters category.
         table.insert(teleporters, { itemID = 17031, itemName = "Rune of Teleportation" })
         table.insert(teleporters, { itemID = 17032, itemName = "Rune of Portals" })
     end
 
-    -- Available in all versions of the game.
-    table.insert(teleporters, { itemID = 6948, itemName = "Hearthstone" })
-    table.insert(teleporters, { itemID = 17690, itemName = "Frostwolf Insignia Rank 1" })
-    table.insert(teleporters, { itemID = 17691, itemName = "Stormpike Insignia Rank 1" })
-    table.insert(teleporters, { itemID = 17900, itemName = "Stormpike Insignia Rank 2" })
-    table.insert(teleporters, { itemID = 17901, itemName = "Stormpike Insignia Rank 3" })
-    table.insert(teleporters, { itemID = 17902, itemName = "Stormpike Insignia Rank 4" })
-    table.insert(teleporters, { itemID = 17903, itemName = "Stormpike Insignia Rank 5" })
-    table.insert(teleporters, { itemID = 17904, itemName = "Stormpike Insignia Rank 6" })
-    table.insert(teleporters, { itemID = 17905, itemName = "Frostwolf Insignia Rank 2" })
-    table.insert(teleporters, { itemID = 17906, itemName = "Frostwolf Insignia Rank 3" })
-    table.insert(teleporters, { itemID = 17907, itemName = "Frostwolf Insignia Rank 4" })
-    table.insert(teleporters, { itemID = 17908, itemName = "Frostwolf Insignia Rank 5" })
-    table.insert(teleporters, { itemID = 17909, itemName = "Frostwolf Insignia Rank 6" })
-    table.insert(teleporters, { itemID = 18149, itemName = "Rune of Recall (Frostwolf Keep)" })
-    table.insert(teleporters, { itemID = 18150, itemName = "Rune of Recall (Dun Baldar)" })
-    table.insert(teleporters, { itemID = 18984, itemName = "Dimensional Ripper - Everlook" })
-    table.insert(teleporters, { itemID = 18986, itemName = "Ultrasafe Transporter - Gadgetzan" })
-    table.insert(teleporters, { itemID = 22589, itemName = "Atiesh, Greatstaff of the Guardian (Mage)" })
-    table.insert(teleporters, { itemID = 22630, itemName = "Atiesh, Greatstaff of the Guardian (Warlock)" })
-    table.insert(teleporters, { itemID = 22631, itemName = "Atiesh, Greatstaff of the Guardian (Priest)" })
-    table.insert(teleporters, { itemID = 22632, itemName = "Atiesh, Greatstaff of the Guardian (Druid)" })
-
-    -- These are available in TBC and later versions of the game.
-    if isCata or isRetail then
+    -- Section Vanilla
+    if isClassicOrAbove then
+        table.insert(teleporters, { itemID = 6948, itemName = "Hearthstone" })
+        table.insert(teleporters, { itemID = 17690, itemName = "Frostwolf Insignia Rank 1" })
+        table.insert(teleporters, { itemID = 17691, itemName = "Stormpike Insignia Rank 1" })
+        table.insert(teleporters, { itemID = 17900, itemName = "Stormpike Insignia Rank 2" })
+        table.insert(teleporters, { itemID = 17901, itemName = "Stormpike Insignia Rank 3" })
+        table.insert(teleporters, { itemID = 17902, itemName = "Stormpike Insignia Rank 4" })
+        table.insert(teleporters, { itemID = 17903, itemName = "Stormpike Insignia Rank 5" })
+        table.insert(teleporters, { itemID = 17904, itemName = "Stormpike Insignia Rank 6" })
+        table.insert(teleporters, { itemID = 17905, itemName = "Frostwolf Insignia Rank 2" })
+        table.insert(teleporters, { itemID = 17906, itemName = "Frostwolf Insignia Rank 3" })
+        table.insert(teleporters, { itemID = 17907, itemName = "Frostwolf Insignia Rank 4" })
+        table.insert(teleporters, { itemID = 17908, itemName = "Frostwolf Insignia Rank 5" })
+        table.insert(teleporters, { itemID = 17909, itemName = "Frostwolf Insignia Rank 6" })
+        table.insert(teleporters, { itemID = 18149, itemName = "Rune of Recall (Frostwolf Keep)" })
+        table.insert(teleporters, { itemID = 18150, itemName = "Rune of Recall (Dun Baldar)" })
+        table.insert(teleporters, { itemID = 18984, itemName = "Dimensional Ripper - Everlook" })
+        table.insert(teleporters, { itemID = 18986, itemName = "Ultrasafe Transporter - Gadgetzan" })
+        table.insert(teleporters, { itemID = 22589, itemName = "Atiesh, Greatstaff of the Guardian (Mage)" })
+        table.insert(teleporters, { itemID = 22630, itemName = "Atiesh, Greatstaff of the Guardian (Warlock)" })
+        table.insert(teleporters, { itemID = 22631, itemName = "Atiesh, Greatstaff of the Guardian (Priest)" })
+        table.insert(teleporters, { itemID = 22632, itemName = "Atiesh, Greatstaff of the Guardian (Druid)" })
+    end
+    -- Section TBC
+    if isTBCOrAbove then
         table.insert(teleporters, { itemID = 28585, itemName = "Ruby Slippers" })
         table.insert(teleporters, { itemID = 29796, itemName = "Socrethar's Teleportation Stone" })
         table.insert(teleporters, { itemID = 30542, itemName = "Dimensional Ripper - Area 52" })
@@ -170,6 +181,9 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 35230, itemName = "Darnarian's Scroll of Teleportation" })
         table.insert(teleporters, { itemID = 37118, itemName = "Scroll of Recall" })
         table.insert(teleporters, { itemID = 37863, itemName = "Direbrew's Remote" })
+    end
+    -- Section WotLK
+    if isWotLKOrAbove then
         table.insert(teleporters, { itemID = 40585, itemName = "Signet of the Kirin Tor" })
         table.insert(teleporters, { itemID = 40586, itemName = "Band of the Kirin Tor" })
         table.insert(teleporters, { itemID = 43824, itemName = "The Schools of Arcane Magic - Mastery (spires atop the Violet Citadel)" })
@@ -194,6 +208,9 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 51560, itemName = "Runed Band of the Kirin Tor" })
         table.insert(teleporters, { itemID = 52251, itemName = "Jaina's Locket" })
         table.insert(teleporters, { itemID = 54452, itemName = "Ethereal Portal" })
+    end
+    -- Section Cataclysm
+    if isCataOrAbove
         table.insert(teleporters, { itemID = 58487, itemName = "Potion of Deepholm" })
         table.insert(teleporters, { itemID = 61379, itemName = "Gidwin's Hearthstone" })
         table.insert(teleporters, { itemID = 63206, itemName = "Wrap of Unity: Stormwind" })
@@ -208,9 +225,6 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 65360, itemName = "Cloak of Coordination: Stormwind" })
         table.insert(teleporters, { itemID = 68808, itemName = "Hero's Hearthstone" })
         table.insert(teleporters, { itemID = 68809, itemName = "Veteran's Hearthstone" })
-    end
-
-    if isCata then
         table.insert(teleporters, { itemID = 184871, itemName = "Dark Portal (TBC)" })
         table.insert(teleporters, { itemID = 199335, itemName = "Teleport Scroll: Menethil Harbor" })
         table.insert(teleporters, { itemID = 199336, itemName = "Teleport Scroll: Stormwind Harbor" })
@@ -218,9 +232,8 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 199778, itemName = "Teleport Scroll: Undercity Zepplin Tower" })
         table.insert(teleporters, { itemID = 200068, itemName = "Teleport Scroll: Shattrath City" })
     end
-
-    -- And these are only in Retail, because classic isn't past Cata yet.
-    if isRetail then
+    -- Section Mists
+    if isMistsOrAbove then
         table.insert(teleporters, { itemID = 87215, itemName = "Wormhole Generator: Pandaria" })
         table.insert(teleporters, { itemID = 87548, itemName = "Lorewalker's Lodestone" })
         table.insert(teleporters, { itemID = 92510, itemName = "Vol'jin's Hearthstone" })
@@ -230,6 +243,9 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 95567, itemName = "Kirin Tor Beacon" })
         table.insert(teleporters, { itemID = 95568, itemName = "Sunreaver Beacon" })
         table.insert(teleporters, { itemID = 103678, itemName = "Time-Lost Artifact" })
+    end
+    -- Section WoD
+    if isRetail then
         table.insert(teleporters, { itemID = 110560, itemName = "Garrison Hearthstone" })
         table.insert(teleporters, { itemID = 112059, itemName = "Wormhole Centrifuge" })
         table.insert(teleporters, { itemID = 117389, itemName = "Draenor Archaeologist's Lodestone" })
@@ -241,6 +257,9 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 128502, itemName = "Hunter's Seeking Crystal" })
         table.insert(teleporters, { itemID = 128503, itemName = "Master Hunter's Seeking Crystal" })
         table.insert(teleporters, { itemID = 128353, itemName = "Admiral's Compass" })
+    end
+    -- Section Legion
+    if isRetail then
         table.insert(teleporters, { itemID = 129276, itemName = "Beginner's Guide to Dimensional Rifting" })
         table.insert(teleporters, { itemID = 132119, itemName = "Orgrimmar Portal Stone" })
         table.insert(teleporters, { itemID = 132120, itemName = "Stormwind Portal Stone" })
@@ -267,6 +286,9 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 144392, itemName = "Pugilist's Powerful Punching Ring (Horde)" })
         table.insert(teleporters, { itemID = 150733, itemName = "Scroll of Town Portal (Ar'gorok in Arathi)" })
         table.insert(teleporters, { itemID = 151652, itemName = "Wormhole Generator: Argus" })
+    end
+    -- Section BFA
+    if isRetail then
         table.insert(teleporters, { itemID = 159224, itemName = "Zuldazar Hearthstone" })
         table.insert(teleporters, { itemID = 160219, itemName = "Scroll of Town Portal (Stromgarde in Arathi)" })
         table.insert(teleporters, { itemID = 162973, itemName = "Greatfather Winter's Hearthstone" })
@@ -287,6 +309,9 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 169064, itemName = "Montebank's Colorful Cloak" })
         table.insert(teleporters, { itemID = 169297, itemName = "Stormpike Insignia" })
         table.insert(teleporters, { itemID = 169862, itemName = "Alluring Bloom" })
+    end
+    -- Section Shadowlands
+    if isRetail then
         table.insert(teleporters, { itemID = 172179, itemName = "Eternal Traveler's Hearthstone" })
         table.insert(teleporters, { itemID = 172203, itemName = "Cracked Hearthstone" })
         table.insert(teleporters, { itemID = 172924, itemName = "Wormhole Generator: Shadowlands" })
@@ -312,6 +337,9 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 190196, itemName = "Enlightened Hearthstone" })
         table.insert(teleporters, { itemID = 190237, itemName = "Broker Translocation Matrix" })
         table.insert(teleporters, { itemID = 191029, itemName = "Lilian's Hearthstone" })
+    end
+    -- Section Dragonflight
+    if isRetail then
         table.insert(teleporters, { itemID = 193000, itemName = "Ring-Bound Hourglass" })
         table.insert(teleporters, { itemID = 193588, itemName = "Timewalker's Hearthstone" })
         table.insert(teleporters, { itemID = 198156, itemName = "Wyrmhole Generator: Dragon Isles" })
@@ -325,18 +353,19 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 205456, itemName = "Lost Dragonscale (1)" })
         table.insert(teleporters, { itemID = 205458, itemName = "Lost Dragonscale (2)" })
         table.insert(teleporters, { itemID = 206195, itemName = "Path of the Naaru" })
-        table.insert(teleporters, { itemID = 208704, itemName = "Deepdweller's Earthen Hearthstone" })
         table.insert(teleporters, { itemID = 209035, itemName = "Hearthstone of the Flame" })
         table.insert(teleporters, { itemID = 210455, itemName = "Draenic Hologem" })
         table.insert(teleporters, { itemID = 211788, itemName = "Tess's Peacebloom" })
         table.insert(teleporters, { itemID = 212337, itemName = "Stone of the Hearth" })
         table.insert(teleporters, { itemID = 219222, itemName = "Time-Lost Artifact (Remix)" })
+    end
+    -- Section The War Within
+    if isRetail then
+        table.insert(teleporters, { itemID = 208704, itemName = "Deepdweller's Earthen Hearthstone" })
         table.insert(teleporters, { itemID = 223988, itemName = "Dalaran Hearthstone (Quest Item)" })
         table.insert(teleporters, { itemID = 228940, itemName = "Notorious Thread's Hearthstone" })
         table.insert(teleporters, { itemID = 228996, itemName = "Relic of Crystal Connections" })
         table.insert(teleporters, { itemID = 235016, itemName = "Redeployment Module" })
-    end
-    if interfaceVersion >= 110100 then
         table.insert(teleporters, { itemID = 230850, itemName = "Delve-O-Bot 7001" })
         table.insert(teleporters, { itemID = 236687, itemName = "Explosive Hearthstone" })
         table.insert(teleporters, { itemID = 234389, itemName = "Gallagio Loyalty Rewards Card: Silver" })
@@ -345,6 +374,11 @@ function Teleporters:addTeleportItemsToTable()
         table.insert(teleporters, { itemID = 234392, itemName = "Gallagio Loyalty Rewards Card: Black" })
         table.insert(teleporters, { itemID = 234393, itemName = "Gallagio Loyalty Rewards Card: Diamond" })
         table.insert(teleporters, { itemID = 234394, itemName = "Gallagio Loyalty Rewards Card: Legendary" })
+    end
+    if interfaceVersion >= 110200 then
+        table.insert(teleporters, { itemID = 243056, itemName = "Delver's Mana-Bound Ethergate" })
+        table.insert(teleporters, { itemID = 245970, itemName = "P.O.S.T. Master's Express Hearthstone" })
+        table.insert(teleporters, { itemID = 246565, itemName = "Cosmic Hearthstone" })
     end
 end
 
